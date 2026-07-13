ae41fcbe6f7b7c9060e688f6d939cda1945ea8c8
ermacv <ermachenkov.v.s@gmail.com>
2026-07-13 17:35:30 +0200

    fix(esp32s31): define referenced FTM calibration data

---
+++ esp-wifi-sys-esp32s31/c/ftm_calibration_data.c
@@ -0,0 +1,33 @@
+#include <stdint.h>
+
+/*
+ * ESP-IDF components/esp_wifi/src/ftm_load_calibration.c data for ESP32-S31.
+ * IDF currently labels these values as provisional; the Wi-Fi blob references
+ * the variables even when the Rust port does not expose FTM operation.
+ */
+uint16_t est_PHY_INIT_FTM_COMP_20_20U_MHZ = 437;
+uint16_t est_PHY_INIT_FTM_COMP_20_20U_MHZ_DIS = 433;
+uint16_t est_PHY_INIT_FTM_COMP_20_20D_MHZ = 443;
+uint16_t est_PHY_INIT_FTM_COMP_20_20D_MHZ_DIS = 442;
+uint16_t est_PHY_RESP_FTM_COMP_20_20U_MHZ = 426;
+uint16_t est_PHY_RESP_FTM_COMP_20_20U_MHZ_DIS = 429;
+uint16_t est_PHY_RESP_FTM_COMP_20_20D_MHZ = 420;
+uint16_t est_PHY_RESP_FTM_COMP_20_20D_MHZ_DIS = 421;
+
+uint16_t est_PHY_INIT_FTM_COMP_20_40U_MHZ = 462;
+uint16_t est_PHY_INIT_FTM_COMP_20_40U_MHZ_DIS = 433;
+uint16_t est_PHY_INIT_FTM_COMP_20_40D_MHZ = 438;
+uint16_t est_PHY_INIT_FTM_COMP_20_40D_MHZ_DIS = 442;
+uint16_t est_PHY_RESP_FTM_COMP_20_40U_MHZ = 425;
+uint16_t est_PHY_RESP_FTM_COMP_20_40U_MHZ_DIS = 430;
+uint16_t est_PHY_RESP_FTM_COMP_20_40D_MHZ = 420;
+uint16_t est_PHY_RESP_FTM_COMP_20_40D_MHZ_DIS = 420;
+
+uint16_t est_PHY_INIT_FTM_COMP_40_40U_MHZ = 237;
+uint16_t est_PHY_INIT_FTM_COMP_40_40U_MHZ_DIS = 237;
+uint16_t est_PHY_INIT_FTM_COMP_40_40D_MHZ = 234;
+uint16_t est_PHY_INIT_FTM_COMP_40_40D_MHZ_DIS = 234;
+uint16_t est_PHY_RESP_FTM_COMP_40_40U_MHZ = 626;
+uint16_t est_PHY_RESP_FTM_COMP_40_40U_MHZ_DIS = 626;
+uint16_t est_PHY_RESP_FTM_COMP_40_40D_MHZ = 629;
+uint16_t est_PHY_RESP_FTM_COMP_40_40D_MHZ_DIS = 629;
